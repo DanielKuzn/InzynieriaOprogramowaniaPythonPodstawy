dddfc4b9b17eb4df20e4ac86316bf71ae3a7a978
DanielKuzn <kuznickidaniel@wp.pl>
2025-04-18 20:35:14 +0200

    Added logic to verify_pesel function

--- pesel.py
+++ pesel.py
@@ -28,15 +28,13 @@ def verify_pesel(pesel: str) -> int:
     Returns:
         int: 1 jeśli numer jest poprawny, 0 jeśli nie.
     """
-    ### TUTAJ PODAJ ROZWIĄZANIE ZADANIA
     import numpy as np
     pesel_weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
     pesel_array = np.array(list(pesel), dtype=int)
-    ### return 0 - powinno być zmienione i zwrócić prawdziwy wynik (zgodny z oczekiwaniami)
-    return 0
+    return int(10 - (pesel_array[0:10]@pesel_weights)%10 == pesel_array[10])
 
 
 # Przykładowe wywołanie:
 if __name__ == "__main__":
-    pesel_input = "97082123152"
+    pesel_input = input("Write PESEL number to verify: ") # "97082123152"
     print(verify_pesel(pesel_input))  # Oczekiwane wyjście: 0
